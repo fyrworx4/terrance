8206a36eff6e6790461bbb404d6134ac69a4a147
Taylor Nguyen <fyrworx@gmail.com>
2022-03-02 23:25:29 -0800

    yay

--- lawrence-kim.py
+++ lawrence-kim.py
@@ -8,4 +8,4 @@ class MyClient(discord.Client):
         print('Message from {0.author}: {0.content}'.format(message))
 
 client = MyClient()
-client.run('OTQ4ODQwNDkyMzE2MTE5MTEw.YiBqkQ.UWTvbNsbdYfI_JPm8l1aBBokKSU')
+client.run('asdf')
